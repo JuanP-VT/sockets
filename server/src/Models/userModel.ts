badba3ef4fc0fe1ef934404877a90c91aa958d72
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-06-14 18:32:32 -0700

    UPD replace common js module with es6 module

--- server/src/Models/userModel.ts
+++ server/src/Models/userModel.ts
@@ -1,10 +1,11 @@
+import mongoose from "mongoose";
 /**
  * Database Schema for user
  */
-const mongoose = require("mongoose");
+
 const UserSchema = new mongoose.Schema({
   name: String,
   password: String,
 });
 
-module.exports = mongoose.model("Users", UserSchema);
+export default mongoose.model("Users", UserSchema);
